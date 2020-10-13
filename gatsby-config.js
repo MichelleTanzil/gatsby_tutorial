6affa372b6b46988042c7a11143396f692d6a795
Michelle Tanzil <mtanzil01@gmail.com>
2020-10-12 18:08:12 -0700

    Added images and source-filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,5 +13,14 @@ module.exports = {
     person: { name: "Michelle", age: "30" },
   },
   /* Your site config here */
-  plugins: [`gatsby-plugin-styled-components`],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 }
