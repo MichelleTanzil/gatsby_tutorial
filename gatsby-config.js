f09f9a453238fed3670d1aa19e064c4d9ae3b988
Michelle Tanzil <mtanzil01@gmail.com>
2020-10-13 20:00:46 -0700

    finished gatsby-image section

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,11 +15,20 @@ module.exports = {
   /* Your site config here */
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
       },
     },
   ],
